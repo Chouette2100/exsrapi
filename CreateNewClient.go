acafc6ff2b3bc8d4fef968c4f4ea36ba7ced7dac
Chouette2100 <chouette.21.00@gmail.com>
2022-08-20 18:27:07 +0900

    1.0.2 ログ出力を削除する（Ver.1.0.1では、ログ出力が残っていた）

--- CreateNewClient.go
+++ CreateNewClient.go
@@ -33,6 +33,7 @@ Ver.0.0.0
 Ver.1.0.0 LoginShowroom()の戻り値 status を err に変更する。
 Ver.-.-.- exsrapi.go から分離する。
 Ver.1.0.1 errの印字をやめ、エラー内容をerrにセットして返すようにする。
+Ver.1.0.2 ログ出力を削除する（Ver.1.0.1では、ログ出力が残っていた）
 
 */
 
